ad75ed670468caa54333cd2ddcdca3ad5c6a6c15
willymomeni7980-cmd <willymomeni7980@gmail.com>
2026-05-27 13:00:15 +0330

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-print("importing config...", flush=True)
-import config
-print("importing database...", flush=True)
-import database as db
-print("init db...", flush=True)
-db.init_db()
-print("ALL OK", flush=True)
